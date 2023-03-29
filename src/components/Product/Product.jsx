eab0fd77215374619904fd639550ac0863adef60
hasanisokay <hasanisokay@gmail.com>
2023-03-29 10:28:58 +0600

    added cart button to cart summary onclick

--- src/components/Product/Product.jsx
+++ src/components/Product/Product.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Product = (props) => {
     const { img, name, price, ratings, seller, category } = props.product
     return (
-        <div>
+        <div className='mb-4'>
             <div className="card shadow-xl w-full h-full">
                 <figure><img src={img} /></figure>
                 <div className="card-body">
@@ -14,7 +14,7 @@ const Product = (props) => {
                         <p>Rating: {ratings} star</p>
                     </div>
                 </div>
-                <button className="btn btn-error">Add to cart 
+                <button className="btn btn-error" onClick={()=>props.handleCart(props.product)} >Add to cart 
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
                     <path strokeLinecap="round" strokeLinejoin="round" d="M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 00-3 3h15.75m-12.75-3h11.218c1.121-2.3 2.1-4.684 2.924-7.138a60.114 60.114 0 00-16.536-1.84M7.5 14.25L5.106 5.272M6 20.25a.75.75 0 11-1.5 0 .75.75 0 011.5 0zm12.75 0a.75.75 0 11-1.5 0 .75.75 0 011.5 0z" />
                 </svg>
--- src/components/Shop/Shop.jsx
+++ src/components/Shop/Shop.jsx
@@ -1,25 +1,37 @@
 import React, { useEffect, useState } from 'react';
 import Product from '../Product/Product';
-
+import "./Shop.css"
 const Shop = () => {
     const [products, setProducts] = useState([])
+    const [cart, setCart] = useState([])
+    console.log(cart)
+    const handleCart =(product)=>{
+        const newCart = [...cart, product]
+        setCart(newCart)
+    }
+
     useEffect(()=>{
         fetch("https://raw.githubusercontent.com/ProgrammingHero1/ema-john-resources/main/fakeData/products.json")
         .then(res=>res.json())
         .then(data=>setProducts(data))
     },[])
-    console.log(products)
+
     return (
-        <div className='shop-container grid grid-cols-4'>
-            <div className="mt-8 w-full gap-4 col-span-3 products-container grid grid-cols-3 mx-4">
+        <div className='mt-8 shop-container flex'>
+            <div className="basis-5/6 w-full gap-4 products-container col-span-3 grid grid-cols-1 lg:grid-cols-3 md:grid-cols-2 mx-4">
                 {
                     products.map(product=> <Product
                     product={product}
                     key={product.id}
+                    handleCart={handleCart}
                     ></Product>)
                 }
             </div>
-            <div className="cart-container"><h4>Order Summary</h4></div>
+            <div className="cart-container bg-primary self-start sticky top-0 h-96 w-60 rounded p-4">
+                <h4>Order Summary</h4>
+                <p>Selected Items: {cart.length}</p>
+                
+            </div>
         </div>
     );
 };
